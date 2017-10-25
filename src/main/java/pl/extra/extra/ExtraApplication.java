190f6b504c24ef198ed27738a6dece08c0d0b892
krystiansola <krystian.sola@amiona.com>
2017-10-25 22:42:21 +0200

    first commit

---
+++ src/main/java/pl/extra/extra/ExtraApplication.java
@@ -0,0 +1,12 @@
+package pl.extra.extra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraApplication.class, args);
+	}
+}
